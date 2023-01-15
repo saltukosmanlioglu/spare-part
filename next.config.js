d29d85704fe2d757298adedfb298c5d7cc58f2a4
Saltuk Osmanlıoğlu <saltuk.osmanlioglu@gmail.com>
2023-01-16 01:03:19 +0300

    bump

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   reactStrictMode: true,
-}
+  pageExtensions: ["next.tsx", "next.ts"],
+  compiler: {
+    relay: {
+      src: "./",
+      language: "typescript",
+    },
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
+  devIndicators: {
+    buildActivity: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
